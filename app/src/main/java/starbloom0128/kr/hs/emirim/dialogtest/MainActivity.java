761d68a58c859ebc245070c51f7e1a82e53d71f4
seungah <fofo2525@naver.com>
2017-05-25 20:55:22 +0900

    #3 Change Items Dialog and Dialog Event Programming

--- app/src/main/java/starbloom0128/kr/hs/emirim/dialogtest/MainActivity.java
+++ app/src/main/java/starbloom0128/kr/hs/emirim/dialogtest/MainActivity.java
@@ -2,23 +2,37 @@ package starbloom0128.kr.hs.emirim.dialogtest;
 
 import android.content.DialogInterface;
 import android.os.Handler;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    String[] itemArr = {"전하영", "사진","뿌려버려","팍씨"};
+    Button butDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button butDialog=(Button) findViewById(R.id.but_dialog);
+        butDialog = (Button) findViewById(R.id.but_dialog);
         butDialog.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-
+        AlertDialog.Builder dialog=new AlertDialog.Builder(this);
+        dialog.setTitle("첫번째 다이얼로그");
+        dialog.setIcon(R.drawable.icon);
+//        dialog.setMessage("여기는 메시지를 쓰는 곳입니다.");
+        dialog.setItems(itemArr, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                butDialog.setText(itemArr[i]);
+            }
+        });
+        dialog.setPositiveButton("OK", null);
+        dialog.show(); //보이게 설정
     }
 }
